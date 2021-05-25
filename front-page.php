7c077382710fa47448ea34f3a9388f33aeeb5e46
Liffindra Angga Zaaldian <lzaaldian@icloud.com>
2021-05-25 21:06:51 +0700

    chore: update to Bootstrap 5

--- front-page.php
+++ front-page.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
 	<main role="main" class="flex-shrink-0">
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"></li>
-				<li data-target="#myCarousel" data-slide-to="2"></li>
-			</ol>
+		<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-indicators">
+				<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
 			<div class="carousel-inner">
 				<div class="carousel-item active">
-					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
+					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
 					<div class="container">
-						<div class="carousel-caption text-left">
+						<div class="carousel-caption text-start">
 							<h1>Example headline.</h1>
 							<p>Some representative placeholder content for the first slide of the carousel.</p>
 							<p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
@@ -20,7 +20,7 @@
 					</div>
 				</div>
 				<div class="carousel-item">
-					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
+					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
 					<div class="container">
 						<div class="carousel-caption">
@@ -31,10 +31,10 @@
 					</div>
 				</div>
 				<div class="carousel-item">
-					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false"><title> </title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"> </text></svg>
+					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
 					<div class="container">
-						<div class="carousel-caption text-right">
+						<div class="carousel-caption text-end">
 							<h1>One more for good measure.</h1>
 							<p>Some representative placeholder content for the third slide of this carousel.</p>
 							<p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
@@ -42,17 +42,22 @@
 					</div>
 				</div>
 			</div>
-			<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+			<button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
 				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+				<span class="visually-hidden">Next</span>
+			</button>
 		</div>
 
+		<!-- Marketing messaging and featurettes
+		================================================== -->
+		<!-- Wrap the rest of the page in another container to center all the content. -->
+
 		<div class="container marketing">
+			<!-- Three columns of text below the carousel -->
 			<div class="row">
 				<div class="col-lg-4">
 					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
@@ -60,23 +65,22 @@
 					<h2>Heading</h2>
 					<p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
 					<p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-				</div>
+				</div><!-- /.col-lg-4 -->
 				<div class="col-lg-4">
 					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
 
 					<h2>Heading</h2>
 					<p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
 					<p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-				</div>
+				</div><!-- /.col-lg-4 -->
 				<div class="col-lg-4">
 					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
 
 					<h2>Heading</h2>
 					<p>And lastly this, the third column of representative placeholder content.</p>
 					<p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-				</div>
+				</div><!-- /.col-lg-4 -->
 			</div>
-
 			<hr class="featurette-divider">
 
 			<div class="row featurette">
@@ -86,10 +90,8 @@
 				</div>
 				<div class="col-md-5">
 					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
 				</div>
 			</div>
-
 			<hr class="featurette-divider">
 
 			<div class="row featurette">
@@ -99,10 +101,8 @@
 				</div>
 				<div class="col-md-5 order-md-1">
 					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
 				</div>
 			</div>
-
 			<hr class="featurette-divider">
 
 			<div class="row featurette">
@@ -112,17 +112,15 @@
 				</div>
 				<div class="col-md-5">
 					<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-
 				</div>
 			</div>
-
 			<hr class="featurette-divider">
 		</div>
 	</main>
 
 	<footer class="footer mt-auto py-3">
 		<div class="container">
-			<p class="float-right"><a href="#">Back to top</a></p>
+			<p class="float-end"><a href="#">Back to top</a></p>
 			<p>&copy; <?php echo date('Y'); ?> Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 		</div>
 	</footer>
